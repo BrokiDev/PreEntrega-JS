d2edbd49b5d8d23e3529e3915e01820d0558b3a8
BrokiDev <bryantramirez855@gmail.com>
2023-05-05 00:37:19 -0400

    En esta entrega solo use condicionales

---
+++ main.js
@@ -0,0 +1,40 @@
+// Calcular costo total de productos y/o servicios seleccionados por el usuario.
+// Calcular pagos en cuotas sobre un monto determinado.
+// Calcular valor final de un producto seleccionado en función de impuestos y descuentos.
+// Calcular tiempo de espera promedio en relación con la cantidad de turnos registrados.
+// Calcular edad promedio de personas registradas.
+// Calcular nota final de alumnos ingresados.
+
+alert("Esta es una lista de servicios que puede pagar: 1= electricidad, 2= agua potable, 3= cable y telefono. Por favor digita con un numero la opcion deseada:")
+
+
+
+let servicio = parseInt(prompt("Que servicio desea Pagar"));
+const itbis = 0.18
+let total_final = 0
+
+switch (servicio) {
+    case  1: 
+        servicio = 4200
+        alert(`El monto a pagar de la electricidad sin itbis es: ${servicio}`)
+        total_final = servicio * itbis + servicio
+        alert(`El costo total del servicio con itbis incluido es: ${total_final}`)
+    break
+
+    case 2:
+        servicio = 2800
+        alert(`El monto a pagar del agua potable es: ${servicio}`)
+        total_final = servicio * itbis + servicio
+        alert(`El costo total del servicio es: ${total_final}`)
+    break
+
+    case 3:
+        servicio = 3600
+        alert(`El monto a pagar del cable y telefono es: ${servicio}`)
+        total_final = servicio * itbis + servicio
+        alert(`El costo total del servicio es: ${total_final}`)
+    break
+
+    default:
+        alert("Esta no es una opcion valida por favor introduce las opciones que salen en el menu")
+}
